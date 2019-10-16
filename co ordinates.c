12dba8f283ea071fd08e12957e16c6a5b23fa4f3
braj5765 <56026596+braj5765@users.noreply.github.com>
2019-10-16 22:05:41 +0530

    Checks co-ordinates

---
+++ co ordinates.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+void main()
+{int x,y;
+printf("enter x,y coordinate ");
+scanf("%d%d",&x,&y);
+if(x==0)
+printf("the coordinate lies on y axis");
+if(y==0)
+printf("the co ordinate lies on x axis");
+if("x==y==0"
+   )
+printf("the point lies on origin");
+
+}
