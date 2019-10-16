60b8487588ba84998e2e5e125a5a7bd1086fd8fe
mrharsh06 <56648371+mrharsh06@users.noreply.github.com>
2019-10-16 23:18:57 +0530

    Add files via upload

---
+++ coding/Area of circle.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#define pi=3.14;
+/*area of a circle*/
+/*created by aman bhai*/
+int main()
+{
+int r;
+float p,a;
+printf("Enter radius of circle");
+scanf("%d",&r);
+p=pi;
+printf("%d is the area of circle",(p*r*r));
+
+
+
+}
