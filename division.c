b89b4aa87d1acf09c92779dbe74a275285d9f649
braj5765 <56026596+braj5765@users.noreply.github.com>
2019-10-16 21:52:00 +0530

    Division

---
+++ division.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/*iron man in jammu*/
+
+void main()
+{
+
+float a,b;
+float c;
+printf("enter 2 numbers a,b");
+scanf("%f%f",&a,&b);
+c=a/b;
+printf ("%f is the quotient when a is divided by b ",c);
+
+}
